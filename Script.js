371e0cd712bb499de828b6f816707b8f8a312d99
Mary Angelene V. Balais <95016909+TheMoreTheMary-er@users.noreply.github.com>
2023-10-09 16:21:19 +0800

    NAVBAR AND HOBBIES SECTION
    Added a responsive NAVBAR. Also laid out my Hobbies Section.

---
+++ Script.js
@@ -0,0 +1,36 @@
+// Smooth scrolling to anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 60, // Adjust the offset as needed
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+//Buttons, Animation/Movement of the Responsive Navigation Menu
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick= ()=>{
+    menu.classList.add("active");
+    menuBtn.classList.add("hide");
+    body.classList.add("disabledScroll");
+    }
+cancelBtn.onclick= ()=>{
+    menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
+    body.classList.remove("disabledScroll");
+    }
+window.onscroll= ()=>{
+    this.scrollY > 20 ? navbar.classList.add("sticky"): navbar.classList.remove("sticky");
+    }
